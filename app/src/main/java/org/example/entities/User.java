25ddf50ed61ec6af245d7e88e78ac9c49525d977
Satwik Das <satwikdas872@gmail.com>
2024-11-14 05:47:17 +0530

    1st Commmit

---
+++ app/src/main/java/org/example/entities/User.java
@@ -0,0 +1,9 @@
+package org.example.entities;
+
+public class User {
+    private String name;
+    private String password;
+    private String hashPassword;
+    private List<Ticket> ticketsBoooked;
+    private String userId;
+}
